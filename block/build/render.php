847efd2622e86286c74da50834962986a0fd828a
ThemeHunk <themehunk@gmail.com>
2026-05-30 18:41:05 +0530

    bild main file

--- block/build/render.php
+++ block/build/render.php
@@ -1,4 +1,7 @@
+
+
 <div <?php echo get_block_wrapper_attributes(); ?>>
+
 <?php
 $formid = isset( $attributes['formid'] ) ? absint( $attributes['formid'] ) : 1;
 echo do_shortcode( '[lead-form form-id=' . $formid . ']' );
